ed7bfa8548abaccf8d0a643f29df055186f4d21b
bop-hack <97530647+bop-hack@users.noreply.github.com>
2022-01-23 11:54:14 +0100

    Update scan_worker.sh

--- lib/scan_worker.sh
+++ lib/scan_worker.sh
@@ -5,10 +5,10 @@
 main() {
 	host=$1
 	timeout=$2
-	#~ bash ./lib/scan_phpmyadmin.sh "$host" "$timeout"
-	#~ bash ./lib/scan_wordpress.sh "$host" "$timeout"
-	#~ bash ./lib/scan_ftp_anonymous.sh "$host" "$timeout"
-	#~ bash ./lib/scan_mysqldumper.sh "$host" "$timeout"
+	bash ./lib/scan_phpmyadmin.sh "$host" "$timeout"
+	bash ./lib/scan_wordpress.sh "$host" "$timeout"
+	bash ./lib/scan_ftp_anonymous.sh "$host" "$timeout"
+	bash ./lib/scan_mysqldumper.sh "$host" "$timeout"
 	bash ./lib/scan_mysql.sh "$host" "$timeout"
     echo "$host" > last.log
 }
